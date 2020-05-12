0f6c25cbece94bef41ceb0f7cb15e9047149f2f2
boogiedev <nguyenw@seattleu.edu>
2020-05-11 20:13:00 -0700

    xfer doc

---
+++ src/credentials/keys.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# Client Credentials
+client_id = 'sJhQjqfGOLBO5m-HhnYEDwTPIFNv2SWBGlhDuXpD9grXLHYBwVXekmsYEzPDqS_s'
+client_secret = 'C5jtH0cPX1rPZvzsSv03A_1RzqLKILPONnXVXN3e0-FEWS2HyeKOysGwLWI3tg-A9HIcNUcrGstkYGPP_dmNQw'
+client_token = 'qQ6NZU3CRsp7LEloaDzT2ejRfP7F7HLVBMd0n116kzjkUkkCsSlN3KNSoHMkcZsF'
