c673e7d7d1867b8b922fe3b2e7dc6899f84e793d
Wesley Nguyen <wesleynguyen@outlook.com>
2020-05-12 17:03:40 -0700

    add tokens

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -12,5 +12,16 @@ from urllib.parse import urlencode
 import re
 
 # Import Custom Modules
-from helpers import *
+
+
+# Create Text Cleaning Function
+def clean_text(df:pd.DataFrame, text_field:str, new_text_field_name:str) -> pd.DataFrame:
+    df_cop = df.copy()
+    cleaned = df_cop[text_field].apply(lambda x: [i.lower() for i in x])
+    cleaned = cleaned.apply(lambda line: [re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem) for elem in line])
+#     # remove numbers
+#     df_cop[new_text_field_name] = df_cop[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
+    df_cop[new_text_field_name] = cleaned
+    return df_cop
+
 
