d0b60948550f99b5af2ff636fab9fdeae00be40f
Wesley Nguyen <wesleynguyen@outlook.com>
2020-05-14 12:42:51 -0700

    start LDA

--- src/dummy_words.py
+++ src/dummy_words.py
@@ -9,14 +9,37 @@ from pprint import pprint
 import string
 from math import log
 import re
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def match_dummies(item:str):
     """Replaces profanity with dummy word"""
     bad_prefixes = ['nig', 'bitc', 'puss', 'fuc', 'shit', 'hoe', 'ass', 'cock', 'fag', 'tits', 'cunt', 'dick', 'piss']
-    replace_prefixes = ['nibba', 'binch', 'boos', 'duck']
+    replace_prefixes = [f"expletive_{i}" for i in range(len(bad_prefixes))]
     for pre, rep in zip(bad_prefixes, replace_prefixes):
         match = r'({bad_prefix})(.*?)\b'.format(bad_prefix=pre)
-        res = re.sub(match, rep, sentence)
-        print(res)
+        item = re.sub(match, rep, item)
+    return item
+
+# Helper function
+def plot_10_most_common_words(count_data, count_vectorizer, title:str='10 most common words'):
+    words = count_vectorizer.get_feature_names()
+    total_counts = np.zeros(len(words))
+    for t in count_data:
+        total_counts+=t.toarray()[0]
+    
+    count_dict = (zip(words, total_counts))
+    count_dict = sorted(count_dict, key=lambda x:x[1], reverse=True)[0:10]
+    words = [w[0] for w in count_dict]
+    counts = [w[1] for w in count_dict]
+    x_pos = np.arange(len(words)) 
+    
+    plt.figure(2, figsize=(15, 15/1.6180))
+    plt.subplot(title=title)
+    sns.set_context("notebook", font_scale=1.75, rc={"lines.linewidth": 2.5})
+    sns.barplot(x_pos, counts, palette='husl')
+    plt.xticks(x_pos, words, rotation=90) 
+    plt.xlabel('words')
+    plt.ylabel('counts')
+    plt.show()
